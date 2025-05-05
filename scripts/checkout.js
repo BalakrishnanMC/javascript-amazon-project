eeaf4d2db6ea066f2b048de741f0ec60911e8926
BalakrishnanMC <balakrishnan.mc22@bitsathy.ac.in>
2025-05-05 18:30:15 +0530

    fix: save data and generating HTML for deliveryOptions

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,10 @@
 import { cart, removeFromCart } from '../data/cart.js'
 import { products } from '../data/products.js';
-import { formatCurrency } from './utils/money.js';
+import formatCurrency from './utils/money.js';
+import dayjs from 'https://esm.sh/dayjs';
+import { deliveryOptions } from '../data/deliveryOptions.js';
+
+//const deliveryDate = dayjs().add(0,'days').format('dddd, MMMM D');
 
 let cartSummaryHTML='';
 
@@ -11,10 +15,18 @@ cart.forEach((cartItem) => {
       matchingItem = product;
     }
   });
+
+  let dateString ='';
+  deliveryOptions.forEach((deliveryOption) => {
+    if(deliveryOption.id === cartItem.deliveryOptionId){
+      dateString = dayjs().add(deliveryOption.deliveryDays,'days').format('dddd, MMMM D');
+    }
+  });
+
   cartSummaryHTML+=`
     <div class="cart-item-container js-cart-container-${matchingItem.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -45,45 +57,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchingItem.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingItem.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingItem.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHTML(matchingItem,cartItem)}
         </div>
       </div>
     </div>
@@ -97,7 +71,31 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
       document.querySelector(`.js-cart-container-${productId}`).remove();
     });
   });
+
+function deliveryOptionsHTML(matchingItem,cartItem){
+  let html='';
+  deliveryOptions.forEach((deliveryOption) => {
+    const dateString = dayjs().add(deliveryOption.deliveryDays,'days').format('dddd, MMMM D');
+    const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`;
+    const isChecked = cartItem.deliveryOptionId === deliveryOption.id;
+    html+=`
+          <div class="delivery-option">
+            <input type="radio" ${isChecked ? 'checked' : ''}
+              class="delivery-option-input"
+              name="delivery-option-${matchingItem.id}">
+            <div>
+              <div class="delivery-option-date">
+                ${dateString}
+              </div>
+              <div class="delivery-option-price">
+                ${priceString} Shipping
+              </div>
+            </div>
+          </div>
+    `;
+  });
+  return html;
+}
