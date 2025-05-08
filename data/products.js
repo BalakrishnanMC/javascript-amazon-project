df59061992eca5f1002b2c992a5633a049805afd
BalakrishnanMC <balakrishnan.mc22@bitsathy.ac.in>
2025-05-08 10:16:11 +0530

    feat: used inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -49,6 +49,23 @@ class Product{
   getId(){
     return `${this.id}`;
   }
+
+  extraInfoHTML(){
+    return ``;
+  }
+}
+
+
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `<a href='${this.sizeChartLink}' target='_blank'>Size chart</a>`;
+  }
 }
 
 export const products = [
@@ -711,6 +728,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
