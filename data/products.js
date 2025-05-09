f6bf6ed26aab8fc6088c90f747f1bdc605738f1a
BalakrishnanMC <balakrishnan.mc22@bitsathy.ac.in>
2025-05-09 22:17:04 +0530

    error handling try catch

--- data/products.js
+++ data/products.js
@@ -78,6 +78,9 @@ export function loadProducts(fun){
     console.log('load products');
     fun();
   });
+  xhr.addEventListener('error',() => {
+    console.log('Unexpected error .Please try again later.');
+  });
   xhr.open('GET','https://supersimplebackend.dev/products');
   xhr.send();
 }
@@ -774,6 +777,8 @@ export function loadProductsFetch(){
       return new Product(productDetails);
     });
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error .Please try again later.');
   });
   return promise;
 }
